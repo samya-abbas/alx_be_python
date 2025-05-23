3336583f66384233c0426d8e00c8ee72f9a2864a
Samya <sumoabbas@gmail.com>
2025-05-23 19:30:41 +0300

    Added a new python file basic_operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,6 +3,6 @@ number2 = 5
 a = number1 + number2
 b = number1 - number2
 c = number1 * number2
-print("Addition of " + number1 + " and " + number2 + " is " + a)
-print("Substraction of " + number1 + " and " + number2 + " is " + b)
-print("Multiplication of "+ number1 + " and " + number2 + " is " + c)
+print("Addition of" , number1 , "and" , number2 , "is" , a)
+print("Substraction of", number1 , "and" , number2 , "is" , b)
+print("Multiplication of" , number1 , "and" , number2 , "is" , c)
