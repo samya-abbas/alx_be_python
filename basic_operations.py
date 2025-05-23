64534de4a4bcc862d5cd33796e30758c4d555ecf
Samya <sumoabbas@gmail.com>
2025-05-23 19:00:53 +0300

    add a new python file basic_operations

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+a = number1 + number2
+b = number1 - number2
+c = number1 * number2
+print("Addition of " + number1 + " and " + number2 + " is " + a)
+print("Substraction of " + number1 + " and " + number2 + " is " + b)
+print("Multiplication of "+ number1 + " and " + number2 + " is " + c)
