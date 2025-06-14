46a5fad2d7a61f325d77c0cf20b699ea40a199c1
Samya <sumoabbas@gmail.com>
2025-06-14 15:33:48 +0300

    Added new python files bank_account and main-0 in programming_paradigm

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+    
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}")
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}")
+            return True
+        else:
+            print("Insufficient funds. Withdrawal failed.")
+            return False
+
+    def display_balance(self):
+        print(f"Current balance: ${self.__account_balance}")
