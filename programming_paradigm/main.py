a37ace95e53280c53da76c1bb875831a36565706
Samya <sumoabbas@gmail.com>
2025-06-14 18:56:39 +0300

    Added new python files robust_division_calculator and main

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -5,12 +5,13 @@ def main():
     if len(sys.argv) != 3:
         print("Usage: python main.py <numerator> <denominator>")
         sys.exit(1)
-
+        
     numerator = sys.argv[1]
     denominator = sys.argv[2]
 
+  
     result = safe_divide(numerator, denominator)
-    print(f"The result of the division is {result}")
+    print(result)
 
 if __name__ == "__main__":
     main()
--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,13 +1,15 @@
 def safe_divide(numerator, denominator):
+  
     try:
-        num = float(numerator)
-        denom = float(denominator)
+        num_float = float(numerator)
+        den_float = float(denominator)
 
-        result = num / denom
-        return result
-
-    except ValueError:
-        return "Error: Please enter numeric values only."
+        result = num_float / den_float
+        return f"The result of the division is {result}"
 
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
+    except ValueError:
+        return "Error: Please enter numeric values only."
+    except Exception as e:
+        return f"An unexpected error occurred: {e}"
